8167cd02bb684ac5f1ba79f58a9b843cec48dc3d
HOME\KIM_JONGIN01 <KIM_JONGIN01@eland.co.kr>
2019-06-21 16:24:44 +0900

    ddd

--- tenine-blog-backend/src/main/java/com/tenine/blog/API/APIController.java
+++ tenine-blog-backend/src/main/java/com/tenine/blog/API/APIController.java
@@ -2,16 +2,21 @@ package com.tenine.blog.API;
 
 import com.tenine.blog.domain.posts.Posts;
 import com.tenine.blog.domain.posts.PostsRepository;
+import com.tenine.blog.domain.posts.PostsSaveDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class APIController {
+
+    Logger log = LoggerFactory.getLogger(APIController.class);
 
     @Autowired
     public PostsRepository postsRepository;
@@ -24,4 +29,18 @@ public class APIController {
         return list;
     }
 
+    @PostMapping("/save")
+    public void saveBlog(HttpServletRequest req
+            , HttpServletResponse res
+            , @RequestBody PostsSaveDto postsSaveDto) {
+        res.setHeader("Access-Control-Allow-Origin" , "*");
+        res.setHeader("Access-Control-Allow-Methods" , "OPTIONS");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        res.setContentType("application/json");
+        log.warn(">> input data posts ::" + postsSaveDto.getTitle());
+        postsRepository.save(postsSaveDto.toEntity());
+
+
+    }
 }
